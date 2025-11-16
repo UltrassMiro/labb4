00c090d92090328efe17e8e376ae6036b8ce39d0
Ultras <UltrasMiro@gmail.com>
2025-11-16 11:39:17 +0200

    added logic and interface

---
+++ Arithmetic.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <iostream>
+#include <functional>
+#include <string>
+#include <cstdint>
+
+using namespace std;
+
+// ---------- EventArgs ----------
+struct OverflowEventArgs {
+    int a, b;
+    long long result;
+    string operation;
+};
+
+// ---------- Interface ----------
+class IArithmetic {
+public:
+    virtual int Add(int a, int b) = 0;
+    virtual int Sub(int a, int b) = 0;
+    virtual int Mul(int a, int b) = 0;
+    virtual ~IArithmetic() {}
+};
+
+// ---------- Class with event ----------
+class Arithmetic : public IArithmetic {
+public:
+    function<void(const Arithmetic*, const OverflowEventArgs&)> Overflow;
+
+    int Add(int a, int b) override;
+    int Sub(int a, int b) override;
+    int Mul(int a, int b) override;
+
+private:
+    bool checkOverflow(long long);
+    void raiseOverflow(int a, int b, long long result, const string& operation);
+};
--- main.cpp
+++ main.cpp
@@ -1,16 +1,96 @@
 #include <iostream>
+#include <vector>
+#include <functional>
+#include "Arithmetic.h"
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+using namespace std;
+
+// ---------- Event Handler ----------
+void OnOverflow(const Arithmetic* sender, const OverflowEventArgs& args) {
+    cout << "\n=== OVERFLOW EVENT ===\n";
+    cout << "Operation: " << args.operation << "\n";
+    cout << "Operands: " << args.a << ", " << args.b << "\n";
+    cout << "Overflowed result: " << args.result << "\n";
+    cout << "=======================\n\n";
+}
+
+// ---------- Function-like “delegate” ----------
+int sum2D(const vector<vector<int>>& arr) {
+    int sum = 0;
+    for (auto& row : arr)
+        for (int x : row) sum += x;
+    return sum;
+}
+
+// ---------- Menu Interface ----------
+void menu() {
+    cout << "=============================\n";
+    cout << "     Arithmetic Program\n";
+    cout << "=============================\n";
+    cout << "1. Add\n";
+    cout << "2. Sub\n";
+    cout << "3. Mul\n";
+    cout << "4. Test 2D array sum (delegate)\n";
+    cout << "0. Exit\n";
+    cout << "Choose: ";
+}
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    Arithmetic a;
+    a.Overflow = OnOverflow;
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
+    int choice;
+
+    // Delegate demonstration
+    function<int(const vector<vector<int>>&)> sumDelegate;
+
+    // Assign lambda
+    sumDelegate = [](const vector<vector<int>>& arr) {
+        int s = 0;
+        for (auto& row : arr)
+            for (int x : row) s += x;
+        return s;
+    };
+
+    while (true) {
+        menu();
+        cin >> choice;
+
+        if (choice == 0) break;
+
+        if (choice == 4) {
+            vector<vector<int>> arr = {{1, 2}, {3, 4}};
+            cout << "Sum by lambda: " << sumDelegate(arr) << "\n";
+
+            // Anonymous function
+            sumDelegate = function<int(const vector<vector<int>>&)>{
+                [](const vector<vector<int>>& a) {
+                    int s = 0;
+                    for (auto& row : a)
+                        for (int x : row) s += x;
+                    return s;
+                }
+            };
+            cout << "Sum by anonymous: " << sumDelegate(arr) << "\n\n";
+            continue;
+        }
+
+        int x, y;
+        cout << "Enter a and b: ";
+        cin >> x >> y;
+
+        try {
+            switch (choice) {
+                case 1: cout << "Result: " << a.Add(x, y) << "\n\n"; break;
+                case 2: cout << "Result: " << a.Sub(x, y) << "\n\n"; break;
+                case 3: cout << "Result: " << a.Mul(x, y) << "\n\n"; break;
+                default: cout << "Invalid choice\n\n";
+            }
+        }
+        catch (...) {
+            cout << "Unexpected error!\n";
+        }
     }
 
     return 0;
-    // TIP See CLion help at <a href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>. Also, you can try interactive lessons for CLion by selecting 'Help | Learn IDE Features' from the main menu.
 }
